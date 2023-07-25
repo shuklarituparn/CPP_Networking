ef6d8c9e19c57ed8e0b34b127f3074188ce60913
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-07-25 14:47:44 +0300

    added code

---
+++ boost.bind/multithread.cpp
@@ -0,0 +1,40 @@
+//
+// Created by rituparn on 24.07.23.
+//
+
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+#include <boost/bind/bind.hpp>
+
+
+
+
+void workerThread(const boost::shared_ptr<boost::asio::io_service>& iosvc, int counter){  //takes an ioservice shared pointer and an integer counter
+   std::cout<<"Counter "<<".\n";
+   iosvc->run();
+   std::cout<<"End. \n";
+}
+
+int main(){
+    boost::shared_ptr<boost::asio::io_service> io_svc(new boost::asio::io_service); //shared pointer to manage the io_service
+    boost::shared_ptr<boost::asio::io_service::work> worker(new boost::asio::io_service::work(*io_svc)); //helps exiting early
+
+    std::cout<<"Press Enter to exit! "<<std::endl;
+    boost::thread_group threads;
+    for (int i = 0; i < 5; ++i) {
+        threads.create_thread(boost::bind(&workerThread, io_svc, i));
+    }
+    std::cin.get();
+    io_svc->stop();
+    threads.join_all();
+    return 0;
+}
+
+//shared pointer is used to manage the ownership of the worker
+
+//Press enter to exit is displayed first
+
+//the threads are created and the main function waits for the user to enter "ENTER"
+
